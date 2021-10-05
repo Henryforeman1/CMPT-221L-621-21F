a3dcccef436994f7b30e97ff5f0bc6e2f3379c03
Henryforeman1 <79278940+Henryforeman1@users.noreply.github.com>
2021-10-05 15:23:05 -0400

    Lab 5

---
+++ Labs/Lab 5/Submission/script.js
@@ -0,0 +1,4 @@
+document.getElementById("header").innerHTML = "Title changed";
+document.getElementById("description").innerHTML = "Henry Foreman";
+document.getElementById("bossy").innerHTML = "Do you mind doing these for me?";
+ 
